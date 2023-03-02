5e28640eba00964c57c6aabf97fb58ff4e59b74c
Alexey <ReetzCraft@gmail.com>
2023-03-02 18:28:59 +0300

    Initial commit

---
+++ src/test/java/me/serebryakov/junittests/JUnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.serebryakov.junittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
